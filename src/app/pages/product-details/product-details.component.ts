885e5d8561e2d94f29cd2da6bdb59a29d3f276a4
AtharvKasar <atharvk752@gmail.com>
2025-06-09 10:57:34 +0530

    update productApiService for single product details in routing

--- src/app/pages/product-details/product-details.component.ts
+++ src/app/pages/product-details/product-details.component.ts
@@ -1,7 +1,9 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, effect, inject, signal } from '@angular/core';
 import { ProductService } from '../../services/product.service';
 import { CommonModule } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
+import { ProductApiService } from '../../services/product-api.service';
+import { Product } from '../../models/products.model';
 
 @Component({
   selector: 'app-product-details',
@@ -11,13 +13,27 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class ProductDetailsComponent {
   productService = inject(ProductService);
+  productApiService = inject(ProductApiService);
   route = inject(ActivatedRoute);
 
   productId = Number(this.route.snapshot.paramMap.get('id'));
-  product = signal(this.productService.getProductById(this.productId));
+  // product = signal(this.productService.getProductById(this.productId));
+  selectedProduct = signal<Product | null>(null);
 
-  title = this.product()?.title;
-  description = this.product()?.description;
-  image = this.product()?.image;
-  price = this.product()?.price;
+  constructor() {
+    this.productApiService.getProductById(this.productId).subscribe({
+      next: (product) => {
+        console.log('Product fetched:', product);
+        this.selectedProduct.set(product);
+      },
+      error: (err) => {
+        console.error('Error fetching product:', err);
+      }
+    });
+  }
+
+  title = this.selectedProduct()?.title;
+  description = this.selectedProduct()?.description;
+  image = this.selectedProduct()?.image;
+  price = this.selectedProduct()?.price;
 }
